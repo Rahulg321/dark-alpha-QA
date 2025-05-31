49b2e298cb0fa2c2af001f4f3012ec518f6a5c13
Rahul Gupta <rg5353070@gmail.com>
2025-06-01 00:46:21 +0530

    changed the resources function

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -1,45 +1,21 @@
-'use server';
+"use server";
 
 import {
   insertResourceSchema,
   resources,
-  embeddings as embeddingsTable
-} from '@/lib/db/schema';
-import { db } from '@/lib/db/queries';
-import { generateEmbeddings } from '../ai/embedding';
+  embeddings as embeddingsTable,
+} from "@/lib/db/schema";
+import { db } from "@/lib/db/queries";
+import { generateEmbeddings } from "../ai/embedding";
 
-
-export const createResource = async (input: {
-    content: string;
-}) => {
+export const createResource = async (input: { content: string }) => {
   try {
     const { content } = insertResourceSchema.parse(input);
 
-    if (!content) {
-      throw new Error('Content is required');
-    }
-
-    const [resource] = await db
-      .insert(resources)
-      .values({ content })
-      .returning();
-
-      console.log("Resource created", resource);
-
-      const embeddings = await generateEmbeddings(content);
-      console.log("Embeddings generated");
-      await db.insert(embeddingsTable).values(
-        embeddings.map(embedding => ({
-          resourceId: resource.id,
-          ...embedding,
-        })),
-      );
-  
-      return 'Resource successfully created and embedded.';
-
+    return "Resource successfully created and embedded.";
   } catch (e) {
     console.error("An Error Occured while creating a resource", e);
     if (e instanceof Error)
-      return e.message.length > 0 ? e.message : 'Error, please try again.';
+      return e.message.length > 0 ? e.message : "Error, please try again.";
   }
 };
