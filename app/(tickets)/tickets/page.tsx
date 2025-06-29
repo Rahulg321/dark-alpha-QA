eea671acbceb2d37545330bd87415dd9d0b35328
Rahul Gupta <rg5353070@gmail.com>
2025-06-29 18:40:13 +0530

    changed theme

--- app/(tickets)/tickets/page.tsx
+++ app/(tickets)/tickets/page.tsx
@@ -15,36 +15,7 @@ export const metadata: Metadata = {
 const TicketsPage = async () => {
   const tickets = await getAllTickets();
 
-  return (
-    <section className="block-space big-container">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Tickets</h1>
-        <div className="flex justify-end">
-          <Button variant="default" className="rounded-full" asChild>
-            <Link href="/tickets/new">
-              <PlusCircle className="size-4" />
-              New Ticket
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 md:mt-8">
-        {tickets.length === 0 && (
-          <div className="col-span-2">
-            <p>No tickets found</p>
-            <Button asChild>
-              <Link href="/tickets/new">Create a new ticket</Link>
-            </Button>
-          </div>
-        )}
-
-        {tickets.length > 0 &&
-          tickets.map((ticket) => (
-            <TicketCard key={ticket.id} ticket={ticket} />
-          ))}
-      </div>
-    </section>
-  );
+  return <section className="block-space big-container"></section>;
 };
 
 export default TicketsPage;
